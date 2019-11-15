b30aee112a2d16a5a6c1d66ad6a9be226e7776e3
Toma4205 <toma.vergnaud@gmail.com>
2019-11-15 11:58:31 +0100

    MyProfilScreen  - Bouton Option

---
+++ lib/model/record.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Record {
+  Record({this.typeYear, this.points, this.rank, this.suffixRank});
+
+  String typeYear;
+  String points;
+  String rank;
+  String suffixRank;
+}
