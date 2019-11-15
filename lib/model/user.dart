8a22baf04ab2ba9c8feaaf7ac96b2757574b0d91
Toma4205 <toma.vergnaud@gmail.com>
2019-11-15 11:59:01 +0100

    MyProfilScreen  - Bouton Option

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:bricktime/model/record.dart';
 
 class User {
-  User({this.id, this.pseudo, this.level, this.lastConnexion});
+  User({this.id, this.pseudo, this.level, this.lastConnexion, this.records, this.avatar});
 
   String id;
   String pseudo;
   String level;
   String lastConnexion;
+  List<Record> records;
+  String avatar;
 }
